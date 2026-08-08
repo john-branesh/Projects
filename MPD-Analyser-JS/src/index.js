92e241976d7d15ce0d15493206fdcc1687aba9c4
John Branesh <johnbraneshherin@gmail.com>
2026-08-08 11:12:48 +0530

    1st component to take file path as input

--- MPD-Analyser-JS/src/index.js
+++ MPD-Analyser-JS/src/index.js
@@ -1,1 +1,14 @@
+import readline from 'readline';
+
 console.log ("MPD Analyzer Started...");
+
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+r1.question("Enter the path to the MPD file: ", (filepath)=>{
+    console.log(`MPD file path: ${filepath}`);
+
+    r1.close();
+})
